b07f758acd603192a104cd81226b7c4b27b86b15
Plover <157053167+coderplover@users.noreply.github.com>
2025-08-21 19:41:54 +0300

    Update contactCard.jsx

--- src/component/contactCard.jsx
+++ src/component/contactCard.jsx
@@ -5,7 +5,7 @@ export default function ContactCard(props) {
     <>
       <a href={props.url} target="_blank">
         <div className="inline-flex items-center ">
-          <div className="backdrop-blur-md bg-zinc-900/80 hover:bg-zinc-950 border-1 border-zinc-900 hover:border-zinc-800 px-2 py-1 rounded-xl duration-300 flex flex-row items-center gap-2 hover:text-violet-600 hover:-translate-y-0.5">
+          <div className="backdrop-blur-md border-zinc-900 duration-300 hover:border-zinc-800 border-2 px-2 py-1 rounded-xl duration-300 flex flex-row items-center gap-2 hover:text-violet-600 hover:-translate-y-0.5">
             <FontAwesomeIcon icon={props.iconstr} />
             <p className="text-xl">{props.name}</p>
           </div>
